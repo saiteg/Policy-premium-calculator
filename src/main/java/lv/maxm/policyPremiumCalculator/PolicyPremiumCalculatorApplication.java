c8f261285e93a61f8e35d0ae6f64a63f769fb90c
MaxM <maxm@mgarson.com>
2020-05-19 20:59:55 +0300

    1 commit

---
+++ src/main/java/lv/maxm/policyPremiumCalculator/PolicyPremiumCalculatorApplication.java
@@ -0,0 +1,13 @@
+package lv.maxm.policyPremiumCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicyPremiumCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicyPremiumCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lv/maxm/policyPremiumCalculator/PolicyPremiumCalculatorApplicationTests.java
@@ -0,0 +1,17 @@
+package lv.maxm.policyPremiumCalculator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicyPremiumCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("PolicyPremiumCalculatorApplicationTests.contextLoads() !!!!!!!!!!!!!!11111111111");
+		assertEquals(2, 2);
+	}
+
+}
